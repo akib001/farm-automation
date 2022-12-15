70500e09e15a2a598d7f21acf90791056c77309e
Akib <akibahmed2011@gmail.com>
2022-12-15 12:34:22 +0600

    routes

---
+++ src/Pages/FarmPage.js
@@ -0,0 +1,114 @@
+import {Box, Card, CircularProgress, Grid, Typography} from "@mui/material";
+import useSWR from 'swr';
+import {format, parseISO} from 'date-fns';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+
+function FarmPage() {
+    const {data: tempData, isLoading: isLoadingTemp} = useSWR(`/farm/temp`);
+    const {data: humidityData, isLoading: isLoadingHumidity} = useSWR(`/farm/humidity`);
+    const {data: moistureData, isLoading: isLoadingMoisture} = useSWR(`/farm/moisture`);
+    const {data: lightData, isLoading: isLoadingLight} = useSWR(`/farm/light`);
+
+    console.log(humidityData?.updatedAt)
+
+    return (
+        <>
+            {(!isLoadingTemp && !isLoadingHumidity && !isLoadingMoisture && !isLoadingLight) ? (
+                <Box sx={{flexGrow: 1, padding: 8}}>
+                    <Grid container spacing={3}>
+                        <Grid item xs={12}>
+                            <Card variant="outlined" sx={{
+                                display: 'flex',
+                                flexDirection: 'column',
+                                border: '1px solid #333',
+                                borderRadius: '10px',
+                                textAlign: 'center',
+                                p: '1rem'
+                            }}>
+                                <Typography variant="h4" gutterBottom>
+                                    Farm Temperature:
+                                </Typography>
+                                <Typography variant="h3" gutterBottom>
+                                    {tempData?.temp}° Celcius
+                                </Typography>
+                                <Typography variant="h5" gutterBottom>
+                                    Updated
+                                    At: {tempData?.updatedAt ? format(new Date(parseISO(tempData?.updatedAt)), 'dd/MM/yy hh:mm a') : ''}
+                                </Typography>
+                            </Card>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Card variant="outlined" sx={{
+                                display: 'flex',
+                                flexDirection: 'column',
+                                border: '1px solid #333',
+                                borderRadius: '10px',
+                                textAlign: 'center',
+                                p: '1rem'
+                            }}>
+                                <Typography variant="h4" gutterBottom>
+                                    Farm Humidity:
+                                </Typography>
+                                <Typography variant="h3" gutterBottom>
+                                    {humidityData?.humidity * 100}% g.m-3
+                                </Typography>
+                                <Typography variant="h5" gutterBottom>
+                                    Updated
+                                    At: {humidityData?.updatedAt ? format(new Date(parseISO(humidityData?.updatedAt)), 'dd/MM/yy hh:mm a') : ''}
+                                </Typography>
+                            </Card>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Card variant="outlined" sx={{
+                                display: 'flex',
+                                flexDirection: 'column',
+                                border: '1px solid #333',
+                                borderRadius: '10px',
+                                textAlign: 'center',
+                                p: '1rem'
+                            }}>
+                                <Typography variant="h4" gutterBottom>
+                                    Farm Moisture:
+                                </Typography>
+                                <Typography variant="h3" gutterBottom>
+                                    {moistureData?.moisture} dewpoint °F
+                                </Typography>
+                                <Typography variant="h5" gutterBottom>
+                                    Updated
+                                    At: {moistureData?.updatedAt ? format(new Date(parseISO(moistureData?.updatedAt)), 'dd/MM/yy hh:mm a') : ''}
+                                </Typography>
+                            </Card>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Card variant="outlined" sx={{
+                                display: 'flex',
+                                flexDirection: 'column',
+                                border: '1px solid #333',
+                                borderRadius: '10px',
+                                textAlign: 'center',
+                                p: '1rem'
+                            }}>
+                                <Typography variant="h4" gutterBottom>
+                                    Farm Light:
+                                </Typography>
+                                <Typography variant="h3" gutterBottom>
+                                    {lightData?.light} Lux
+                                </Typography>
+                                <Typography variant="h5" gutterBottom>
+                                    Updated At: {lightData?.updatedAt ? format(new Date(parseISO(lightData?.updatedAt)), 'dd/MM/yy hh:mm a') : ''}
+                                </Typography>
+                            </Card>
+                        </Grid>
+                    </Grid>
+                </Box>
+            ) : (
+                <Box sx={{width: '100wh', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                    <CircularProgress />
+                </Box>
+            )}
+
+        </>
+    );
+}
+
+export default FarmPage;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import axios from 'axios';
 import {SWRConfig} from 'swr';
+import {BrowserRouter} from "react-router-dom";
 
 axios.defaults.baseURL = process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : 'https://farmautomationserver.cyclic.app';
 
@@ -12,12 +11,10 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <SWRConfig value={{fetcher: (url) => axios(url).then((r) => r.data)}}>
-            <App/>
+            <BrowserRouter>
+                <App/>
+            </BrowserRouter>
         </SWRConfig>
     </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
