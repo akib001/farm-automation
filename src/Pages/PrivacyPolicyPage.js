d5628ac1c7e0ef0d151404294fb5c32624431d11
akib <akibahmed2011@gmail.com>
2023-01-30 13:27:50 +0600

    PrivacyPolicyPage.js line break added

--- src/Pages/PrivacyPolicyPage.js
+++ src/Pages/PrivacyPolicyPage.js
@@ -4,45 +4,44 @@ import Typography from "@mui/material/Typography";
 
 const PrivacyPolicyPage = () => {
     return (
-        <Container maxWidth={'xl'}>
+        <Container maxWidth={'lg'}>
             <Typography variant={'h3'} my={3} textAlign={'center'}>Privacy Policy</Typography>
-            <Typography variant={'body1'}>
-                Our Privacy Policy explains:
-                What information we collect and why we collect it.
-                How we use that information.
-                Information that we collect
+            <Typography variant={'body1'} component={'p'} sx={{lineHeight: '1.7 !important'}}>
+                Our Privacy Policy explains: <br style={{ marginBottom: '1rem'}} />
+                What information we collect and why we collect it. <br style={{ marginBottom: '1rem'}} />
+                How we use that information. <br style={{ marginBottom: '1rem'}} />
+                Information that we collect <br style={{ marginBottom: '1rem'}} />
+                We collect information to provide better services to all of our users. Information that we get about you from other services Such as Google Analytics, Firebase, Crashlytics, Google Play and others services we use to improve our applications and services. <br style={{ marginBottom: '1rem'}} />
 
-                We collect information to provide better services to all of our users. Information that we get about you from other services Such as Google Analytics, Firebase, Crashlytics, Google Play and others services we use to improve our applications and services.
+                If you choose to use our Service, then you agree to the collection and use of information in relation with this policy. The Personal Information that [I|we] collect are used for providing and improving the Service. We will not use or share your information with anyone except as described in this Privacy Policy. <br style={{ marginBottom: '1rem'}} />
 
-                If you choose to use our Service, then you agree to the collection and use of information in relation with this policy. The Personal Information that [I|we] collect are used for providing and improving the Service. We will not use or share your information with anyone except as described in this Privacy Policy.
+                This information includes: Information you give us - for example, if our application request login or register to store or sync your date - we’ll ask for personal information, like your name, email address, pictures, location or telephone number. Or will asked you to sign in into your Google (maybe Facebook or other accounts). The information that we request is retained on your device and is not collected by us in any way will be retained by us and used as described in this privacy policy. We do not collect any personal information in our applications for kids. In our kids apps users could access the app or game in its entirety without any signing. <br style={{ marginBottom: '1rem'}} />
 
-                This information includes: Information you give us - for example, if our application request login or register to store or sync your date - we’ll ask for personal information, like your name, email address, pictures, location or telephone number. Or will asked you to sign in into your Google (maybe Facebook or other accounts). The information that we request is retained on your device and is not collected by us in any way will be retained by us and used as described in this privacy policy. We do not collect any personal information in our applications for kids. In our kids apps users could access the app or game in its entirety without any signing.
+                Device information - device-specific information such as your hardware model, operating system version, unique device identifiers and mobile network information. <br style={{ marginBottom: '1rem'}} />
 
-                Device information - device-specific information such as your hardware model, operating system version, unique device identifiers and mobile network information.
+                Log information - this may include: details of how you used our applications or services; internet protocol address; device event information, such as crashes, system activity, hardware settings, browser type, browser language, the date and time of your request and referral URL; cookies that may uniquely identify your browser or your Account. <br style={{ marginBottom: '1rem'}} />
 
-                Log information - this may include: details of how you used our applications or services; internet protocol address; device event information, such as crashes, system activity, hardware settings, browser type, browser language, the date and time of your request and referral URL; cookies that may uniquely identify your browser or your Account.
+                Location information - some of services that we use in the applications (Google Analytics for example) may collect and process information about your location. All data about your location is aggregated non-personally identifiable information. <br style={{ marginBottom: '1rem'}} />
 
-                Location information - some of services that we use in the applications (Google Analytics for example) may collect and process information about your location. All data about your location is aggregated non-personally identifiable information.
+                Unique application numbers - certain services include a unique application number. This number and information about your installation (for example, the operating system type and application version number) <br style={{ marginBottom: '1rem'}} />
 
-                Unique application numbers - certain services include a unique application number. This number and information about your installation (for example, the operating system type and application version number)
+                Local storage - we may collect and store information (including personal information) locally on your device using mechanisms such as browser web storage (including HTML 5) and application data caches. Our apps might allow to download images and set them as wallpaper. <br style={{ marginBottom: '1rem'}} />
+                Cookies and anonymous identifiers - we use various technologies to collect and store information when you visit our site or work with application, which may include one or more cookies or anonymous identifiers sent to your device. We also use cookies and anonymous identifiers when you interact with services that we offer to our partners, such as advertising services and others. <br style={{ marginBottom: '1rem'}} />
 
-                Local storage - we may collect and store information (including personal information) locally on your device using mechanisms such as browser web storage (including HTML 5) and application data caches. Our apps might allow to download images and set them as wallpaper.
-                Cookies and anonymous identifiers - we use various technologies to collect and store information when you visit our site or work with application, which may include one or more cookies or anonymous identifiers sent to your device. We also use cookies and anonymous identifiers when you interact with services that we offer to our partners, such as advertising services and others.
+                Service Providers - We may employ third-party companies and individuals due to the following reasons: <br style={{ marginBottom: '1rem'}} />
+                To facilitate our Service; <br style={{ marginBottom: '1rem'}} />
+                To provide the Service on our behalf; <br style={{ marginBottom: '1rem'}} />
+                To perform Service-related services; or <br style={{ marginBottom: '1rem'}} />
+                To assist us in analyzing how our Service is used. <br style={{ marginBottom: '1rem'}} />
+                We want to inform users of this Service that these third parties have access to your Personal Information. The reason is to perform the tasks assigned to them on our behalf. However, they are obligated not to disclose or use the information for any other purpose. <br style={{ marginBottom: '1rem'}} />
 
-                Service Providers - We may employ third-party companies and individuals due to the following reasons:
-                To facilitate our Service;
-                To provide the Service on our behalf;
-                To perform Service-related services; or
-                To assist us in analyzing how our Service is used.
-                We want to inform users of this Service that these third parties have access to your Personal Information. The reason is to perform the tasks assigned to them on our behalf. However, they are obligated not to disclose or use the information for any other purpose.
+                Security - We value your trust in providing us your Personal Information, thus we are striving to use commercially acceptable means of protecting it. But remember that no method of transmission over the internet, or method of electronic storage is 100% secure and reliable, and we cannot guarantee its absolute security. <br style={{ marginBottom: '1rem'}} />
 
-                Security - We value your trust in providing us your Personal Information, thus we are striving to use commercially acceptable means of protecting it. But remember that no method of transmission over the internet, or method of electronic storage is 100% secure and reliable, and we cannot guarantee its absolute security.
+                Links to Other Sites - This Service may contain links to other sites. If you click on a third-party link, you will be directed to that site. Note that these external sites are not operated by us. Therefore, I strongly advise you to review the Privacy Policy of these websites. I have no control over, and assume no responsibility for the content, privacy policies, or practices of any third-party sites or services. <br style={{ marginBottom: '1rem'}} />
 
-                Links to Other Sites - This Service may contain links to other sites. If you click on a third-party link, you will be directed to that site. Note that these external sites are not operated by us. Therefore, I strongly advise you to review the Privacy Policy of these websites. I have no control over, and assume no responsibility for the content, privacy policies, or practices of any third-party sites or services.
+                Children’s Privacy - This Services do not address anyone under the age of 13. We do not knowingly collect personal identifiable information from children under 13. In the case we discover that a child under 13 has provided us with personal information, we immediately delete this from our servers. If you are a parent or guardian and you are aware that your child has provided us with personal information, please contact us so that we will be able to do necessary actions. <br style={{ marginBottom: '1rem'}} />
 
-                Children’s Privacy - This Services do not address anyone under the age of 13. We do not knowingly collect personal identifiable information from children under 13. In the case we discover that a child under 13 has provided us with personal information, we immediately delete this from our servers. If you are a parent or guardian and you are aware that your child has provided us with personal information, please contact us so that we will be able to do necessary actions.
-
-                Changes to This Privacy Policy - We may update our Privacy Policy from time to time. Thus, you are advised to review this page periodically for any changes. We will notify you of any changes by posting the new Privacy Policy on this page. These changes are effective immediately, after they are posted on this page.
+                Changes to This Privacy Policy - We may update our Privacy Policy from time to time. Thus, you are advised to review this page periodically for any changes. We will notify you of any changes by posting the new Privacy Policy on this page. These changes are effective immediately, after they are posted on this page. <br style={{ marginBottom: '1rem'}} />
 
                 Contact Us - If you have any questions or suggestions about our Privacy Policy, do not hesitate to contact us.
             </Typography>
